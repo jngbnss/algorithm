63ae9208031b8ad46dc56687554186995864ee94
jngbnss <jngbnmn@naver.com>
2025-10-21 14:39:25 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,10 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int size = num2-num1+1;
-        int[] answer = new int[size];
-        for(int i=num1;i<=num2;i++){
-            answer[i-num1] = numbers[i];
-        }
+        int[] answer = {};
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        for(int i = num1; i <= num2; i++)
+            al.add(numbers[i]);
+
+        answer = new int[al.size()];
+        for(int i = 0; i < answer.length; i++)
+            answer[i] = al.get(i);
+
         return answer;
     }
 }
